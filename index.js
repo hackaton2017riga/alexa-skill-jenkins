40b7e773ed5b035cc8a409f11aad8c9f214260ba
ir75 <gines75@gmail.com>
2017-02-10 05:45:26 +0200

    updated index.json

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var http = require('http');
-var config = require('./config.json');
+'use strict';
+//var unirest = require('unirest');
 
-var treeParams = '/api/json?pretty=true&tree=jobs[name,lastBuild[number,timestamp,result,changeSet[items[author[fullName]]]]]';
-var jenkinsUrl = config.jenkinsUrl;
-var username = config.jenkinsUser;
-var apiKey = config.jenkinsApiToken;
-var jobName = config.jenkinsJobName;
-var jenkinsPort = config.jenkinsPort;
+var http = require('http');
+//var treeParams = '/api/json?pretty=true&tree=jobs[name,lastBuild[number,timestamp,result,changeSet[items[author[fullName]]]]]';
+var jenkinsUrl = "bpukdkak:DevOpsRig01@52.56.148.0/jenkins/";
+var username = "jenkins";
+var apiKey = "prUzetHAb2yaPrb";
+var jobName = "Reference_Application_Build";
+var jenkinsPort = 80;
 
 exports.handler = function (event, context) {
     
@@ -23,25 +24,117 @@ exports.handler = function (event, context) {
     }
 };
 
-function onIntent(intentRequest, session, callback){
-    console.log("onIntent requestId=" + intentRequest.requestId + ", sessionId=" + session.sessionId);
-    if ("WhoBrokeTheBuild" === intentRequest.intent.name) {        
-        whoBrokeTheBuild(intentRequest.intent, session, callback);
-    }
+// --------------- Helpers that build all of the responses -----------------------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: 'PlainText',
+            text: output,
+        },
+        card: {
+            type: 'Simple',
+            title: `SessionSpeechlet - ${title}`,
+            content: `SessionSpeechlet - ${output}`,
+        },
+        reprompt: {
+            outputSpeech: {
+                type: 'PlainText',
+                text: repromptText,
+            },
+        },
+        shouldEndSession,
+    };
 }
 
-function whoBrokeTheBuild(intent, session, callback){
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: '1.0',
+        sessionAttributes,
+        response: speechletResponse,
+    };
+}
 
-    console.log("On whoBrokeTheBuild");
 
-    var req = http.get({
-        host: jenkinsUrl,
-        port: jenkinsPort,
-        path: treeParams,
-        auth: username + ':' + apiKey
-    }, function(res) {
+// --------------- Functions that control the skill's behavior -----------------------
 
-        console.log("Returned response from jenkins");
+function getWelcomeResponse(callback) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    const sessionAttributes = {};
+    const cardTitle = 'Welcome';
+    const speechOutput = 'Welcome to the DevOps Hackathon Jenkins skill set. ' +
+        'Please tell me a Jenkins command to execute';
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    const repromptText = 'Please tell me a Jenkins command to execute';
+    const shouldEndSession = false;
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function handleSessionEndRequest(callback) {
+    const cardTitle = 'Session Ended';
+    const speechOutput = 'Thank you for trying the DevOps Hackathon Jenkins skill set.';
+    // Setting this to true ends the session and exits the skill.
+    const shouldEndSession = true;
+
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
+}
+
+function createCommandAttributes(jenkinsCommand) {
+    return {
+        jenkinsCommand,
+    };
+}
+
+function startPipeline (intent, session, callback) {
+    console.log("enter StartPipeline");
+    
+    //var url = "http://bpukdkak:DevOpsRig01@52.56.148.0/jenkins/job/ExampleWorkspace/job/ExampleProject/view/Java_Reference_Application/job/Reference_Application_Build/build?token=prUzetHAb2yaPrb";
+    //var params = "lorem=ipsum&name=alpha";
+
+    var post_data = JSON.stringify("");
+    
+    var post_options = {
+        host: 'bpukdkak:DevOpsRig01@52.56.148.0',
+        port: '80',
+        path: '/jenkins/job/ExampleWorkspace/job/ExampleProject/view/Java_Reference_Application/job/Reference_Application_Build/build?token=prUzetHAb2yaPrb',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': post_data.length
+        }
+    };
+    
+    console.log("10");
+    
+    var post_req = http.request(post_options, function(res) {
+console.log("20");
+        res.setEncoding('utf8');
+        var file = "";
+        //res.pipe(file);
+
+        res.on('response', function(response)  {
+            console.log(response); 
+        });
+console.log("30");
+        res.on('error', function(e) {
+            context.fail('error:' + e.message);
+        })
+console.log("40");
+        res.on('end', function() {
+            context.succeed('success, end request listener');
+        });
+    });
+    
+    ////Send the proper header information along with the request
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xhr.send(params);
+
+    var req = http.post("http://bpukdkak:DevOpsRig01@52.56.148.0/jenkins/job/ExampleWorkspace/job/ExampleProject/view/Java_Reference_Application/job/Reference_Application_Build/build?token=prUzetHAb2yaPrb"
+    , function(res) {
 
         var jenkinsJson = '';
         var cardTitle = intent.name;
@@ -53,6 +146,7 @@ function whoBrokeTheBuild(intent, session, callback){
         res.setEncoding('utf8');
         res.on('data', function(chunk) {
             jenkinsJson += chunk;
+            console.log("chunk=" + chunk);
         });
 
         res.on('end', function() {
@@ -66,6 +160,7 @@ function whoBrokeTheBuild(intent, session, callback){
             }
 
             var lastBuild = desiredJob.lastBuild;
+            
             if(lastBuild.result === 'FAILURE'){
                 var author = getAuthorFromLastBuild(desiredJob.lastBuild);
                 speechOutput = author + " broke the build";
@@ -106,31 +201,118 @@ function getAuthorFromLastBuild(lastBuild){
     return null;
 }
 
-function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
-        },
-        card: {
-            type: "Simple",
-            title: "SessionSpeechlet - " + title,
-            content: "SessionSpeechlet - " + output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
-        },
-        shouldEndSession: shouldEndSession
+/**
+ * Sets the command in the session and prepares the speech to reply to the user.
+ */
+function triggerCommand(intent, session, callback) {
+    const cardTitle = intent.name;
+    const jenkinsCommandSlot = intent.slots.Command;
+    let repromptText = '';
+    let sessionAttributes = {};
+    const shouldEndSession = false;
+    let speechOutput = '';
+
+    if (jenkinsCommandSlot) {
+        const jenkinsCommand = jenkinsCommandSlot.value;
+        sessionAttributes = createCommandAttributes(jenkinsCommand);
+        if (jenkinsCommand === 'start') {
+            speechOutput = `Okay. Starting pipeline.`;
+            startPipeline(intent, session, callback);
+            const shouldEndSession = true;
+        } else if (jenkinsCommand == 'stop') {
+            speechOutput = `Oh, stopping right away your pipeline!`;
+            const shouldEndSession = true;
+        } else if (jenkinsCommand == 'provide status') {
+            const commandResult = "Running!";
+            speechOutput = `The status of pipeline is ${commandResult}`;
+            const shouldEndSession = true;
+        }
+        const shouldEndSession = true;
+        callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
+    } else {
+        speechOutput = "Sorry, I don't understand. Could you please repeat the command?";
+        repromptText = "Are you deaf? Repeat command please. Alexa is waiting for command to start, stop or provide status.";
     }
 }
 
-function buildResponse(sessionAttributes, speechletResponse) {
-    return {
-        version: "1.0",
-        sessionAttributes: sessionAttributes,
-        response: speechletResponse
+// --------------- Events -----------------------
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
+}
+
+/**
+ * Called when the user launches the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
+
+    // Dispatch to your skill's launch.
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
+
+    const intent = intentRequest.intent;
+    const intentName = intentRequest.intent.name;
+
+    // Dispatch to your skill's intent handlers
+    if (intentName === 'JenkinsIntent') {
+        triggerCommand(intent, session, callback);
+    } else if (intentName === 'AMAZON.HelpIntent') {
+        getWelcomeResponse(callback);
+    } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
+        handleSessionEndRequest(callback);
+    } else {
+        throw new Error('Invalid intent');
     }
 }
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
+    // Add cleanup logic here
+}
+
+
+// --------------- Main handler -----------------------
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = (event, context, callback) => {
+    try {
+        console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
+        if (event.session.new) {
+            onSessionStarted({ requestId: event.request.requestId }, event.session);
+        }
+
+        if (event.request.type === 'LaunchRequest') {
+            onLaunch(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'IntentRequest') {
+            onIntent(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'SessionEndedRequest') {
+            onSessionEnded(event.request, event.session);
+            callback();
+        }
+    } catch (err) {
+        callback(err);
+    }
+};
